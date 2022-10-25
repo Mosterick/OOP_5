5c259deb15862cd518acc1cc18b55a77a41cd4de
Matej Novak <mat.nov99@seznam.cz>
2022-10-25 19:48:37 +0200

    Add FizBuz game to OOP_5CV.py

--- OOP_5CV.py
+++ OOP_5CV.py
@@ -1,2 +1,16 @@
 
 print("Hello world and welcome to the game FizBuz")
+print()
+
+x = 50
+for i in range (1,x+1):
+    if i % 15 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+    i = i + 1
+print("Game finished")
